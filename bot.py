0930e212b5088a637d978ded5d6f5d87f3918b3e
kushal <kushaldeveloper@gmail.com>
2020-05-06 11:02:49 -0600

    move down

--- bot.py
+++ bot.py
@@ -1,49 +1,89 @@
 #!/usr/bin/env python3
 
 import os, time, random, requests
+from dataclasses import dataclass
+
+
+@dataclass
+class BoardPosition:
+    x: int
+    y: int
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def get_neighbors(self, boardX: int, boardY: int):
+        result = []
+        if x > 0:
+            result.append(BoardPosition(x=x - 1, y=y))
+        if y > 0:
+            result.append(BoardPosition(x=x, y=y - 1))
+        if x < boardX:
+            result.append(BoardPosition(x + 1, y))
+        if y < boardY:
+            result.append(BoardPosition(x, y + 1))
+        return result
+
 
 lastMove = "up"
-opposites = {"left":"right", "right":"left", "up":"down", "down":"up"}
+opposites = {"left": "right", "right": "left", "up": "down", "down": "up"}
+
 while True:
     try:
-        x = int(requests.get(
-            "https://raw.githubusercontent.com/programical/gitland/master/players/9034725985/x",
-            headers={"Cache-Control": "no-cache", "Pragma": "no-cache"}
-        ).text)
+        x = int(
+            requests.get(
+                "https://raw.githubusercontent.com/programical/gitland/master/players/9034725985/x",
+                headers={"Cache-Control": "no-cache", "Pragma": "no-cache"},
+            ).text
+        )
 
-        y = int(requests.get(
-            "https://raw.githubusercontent.com/programical/gitland/master/players/9034725985/y",
-            headers={"Cache-Control": "no-cache", "Pragma": "no-cache"}
-        ).text)
+        y = int(
+            requests.get(
+                "https://raw.githubusercontent.com/programical/gitland/master/players/9034725985/y",
+                headers={"Cache-Control": "no-cache", "Pragma": "no-cache"},
+            ).text
+        )
         my_map = requests.get(
             "https://raw.githubusercontent.com/programical/gitland/master/map",
-            headers={"Cache-Control": "no-cache", "Pragma": "no-cache"}
+            headers={"Cache-Control": "no-cache", "Pragma": "no-cache"},
         ).text
-        array_from_map = [s.split(',') for s in my_map.split('\n')]
+        array_from_map = [s.split(",") for s in my_map.split("\n")]
+        currentPosition = BoardPosition(x, y)
+        neighbors = currentPosition.get_neighbors(
+            len(array_from_map), len(array_from_map[0])
+        )
     except Exception as err:
         print(str(err))
+        open("error", "a").write(str(err))
         continue
 
-    if x < 5:
+    if currentPosition.x < 5:
         move = "right"
-    elif x > 17:
+    elif currentPosition.x > 17:
         move = "left"
-    elif y < 5:
+    elif currentPosition.y < 5:
         move = "down"
-    elif y > 17:
+    elif currentPosition.y > 17:
         move = "up"
     else:
-        allowedMoves = "right left up down".replace(opposites[lastMove], "").replace("  ", " ").strip()
+        allowedMoves = (
+            "right left up down".replace(opposites[lastMove], "")
+            .replace("  ", " ")
+            .strip()
+        )
         print("since the last move was " + lastMove + ", picking from " + allowedMoves)
         move = random.choice(allowedMoves.split(" "))
-        
+
     open("act", "w").write(move)
     open("map", "w").write(repr(array_from_map))
     status = array_from_map[x][y]
+    open("neighbors", "w").write(repr(time.time()))
+    for neighbor in neighbors:
+        open("neighbors", "a").write(f"({neighbor.x}, {neighbor.y})")
     os.system("git add -A")
-    os.system("git commit -m \"move " + open("act").read().strip() + "\"")
+    os.system('git commit -m "move ' + open("act").read().strip() + '"')
     os.system("git push origin master")
 
     lastMove = move
     time.sleep(60)
-
